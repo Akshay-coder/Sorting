620e6bab7a484c883bedd9181cc93c1b3a5718ef
programmingmind1997 <53514974+programmingmind1997@users.noreply.github.com>
2019-12-07 01:30:53 +0530

    Add files via upload

---
+++ SelectionSort.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+SelectionSort(int *arr,int size)
+{
+        int j=0,i=0,temp=0,iMin=0;
+
+        for(int i=0;i<size-1;i++)
+        {
+		iMin=i;
+                for(j=i+1;j<size;j++)
+			
+		{
+                        if(arr[j]<arr[iMin])
+                        {
+				iMin=j;
+
+                               
+			}
+                }
+
+		if(i!=iMin)
+		{
+			temp=arr[i];
+			arr[i]=arr[iMin];
+			arr[iMin]=temp;
+        }
+	}
+
+}
+
+
+
+int main()
+{
+        int *p=NULL;
+        int isize=0,i=0;
+
+
+        printf("Enter the No of elements to sort");
+        scanf("%d",&isize);
+
+        p=(int *)malloc(isize* sizeof(int));
+
+        printf("Enter the elements");
+
+        for(i=0;i<isize;i++)
+        {
+                scanf("%d",&p[i]);
+        }
+
+
+//      BubbleSort(p,isize);
+	SelectionSort(p,isize);
+
+        printf("Sorted elements are");
+
+        for(i=0;i<isize;i++)
+        {
+                printf("        %d",p[i]);
+        }
+
+        free(p);
+}
+
+     
